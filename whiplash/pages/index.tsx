002ccf9861dffbe7f9de1cee50f5869eddf296eb
d87 <d87d87@gmail.com>
2019-01-04 20:01:06 +0700

    fixed pages

--- whiplash/pages/index.tsx
+++ whiplash/pages/index.tsx
@@ -1,26 +1,3 @@
-import React from "react"
-import App from "../src/components/App/App"
-import {Layout}  from "../src/components/Layout/Layout"
-// import { withApollo } from "react-apollo";
-import { BrowserRouter, Router } from "react-router-dom"
-import { history } from "../src/history"
-import { getTasks, subscribeToResets } from '../src/api/api'
+import TasksPage from './tasks'
 
-class Index extends React.Component {
-    // static async getInitialProps(pageContext) {
-    //     const { req, res, store, isServer } = pageContext
-    //     // console.log(Object.keys(pageContext))
-
-    //     return { }
-    // }
-
-    render() {
-        return (            
-            <Layout>
-                <App />
-            </Layout>
-        )
-    }
-}
-
-export default Index
+export default TasksPage
--- whiplash/pages/login.tsx
+++ whiplash/pages/login.tsx
@@ -3,18 +3,10 @@ import App from "../src/components/App/App"
 import { Layout } from "../src/components/Layout/Layout"
 import LoginPage from "../src/components/LoginPage/LoginPage"
 
-class Index extends React.Component {
-    static async getInitialProps(pageContext) {
-        const { req, res, store, isServer } = pageContext
-    }
-
-    render() {
-        return (
-            <Layout>
-                <LoginPage />
-            </Layout>
-        )
-    }
+export default () => {
+    return (
+        <Layout>
+            <LoginPage />
+        </Layout>
+    )
 }
-
-export default Index
